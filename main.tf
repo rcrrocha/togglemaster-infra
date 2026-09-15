a9871a0111cb0feaf3fc66959ddc77ced00df96a
rcrrocha <rcrrocha@icloud.com>
2026-09-15 18:08:20 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
-resource "aws_s3_bucket" "teste" {
-  bucket = "rocha-fiap-teste-574349219641"
+resource "aws_vpc" "togglemaster" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "togglemaster-vpc"
+  }
 }
