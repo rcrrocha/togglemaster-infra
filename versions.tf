f4c023d08315a72e7139c8de2749c7124282859e
rcrrocha <rcrrocha@icloud.com>
2026-09-15 15:57:50 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,1 +1,9 @@
-# Versoes do Terraform e providers
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
