cb9f136fccdce0a9f545ddd491dda32a7827fa1c
rcrrocha <rcrrocha@icloud.com>
2026-09-15 16:01:20 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+  bucket = "fiap-tfstate-rocha-574349219641"
+  key = "prod/terraform.tfstate"
+  region = "us-east-1"
+  encrypt = true
+  use_lockfile = true
+  }
+}
